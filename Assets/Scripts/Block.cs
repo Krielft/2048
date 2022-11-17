3b43c6536feb97f50b0b72aa8e8fb0a8454660b2
Galaan <galaan.rouzier@e-artsup.net>
2022-11-17 20:56:29 +0100

    .

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int Value;
+    public Vector2 Pos => transform.position;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private TextMeshPro _text;
+    public void Init(BlockType type)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Value = type.Value;
+        _renderer.color = type.Color;
+        _text.text = type.Value.ToString();
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,45 +12,125 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Node _nodePrefab;
     [SerializeField] private Block _blockPrefab;
     [SerializeField] private SpriteRenderer _boardPrefab;
+    [SerializeField] private List<BlockType> _types;
 
     private List<Node> _nodes;
     private List<Block> _blocks;
+    private GameState _state;
+    private int _round;
+
+    private BlockType GetBlockTypeByValue(int value) => _types.First(t => t.Value == value);
 
     private void Start()
     {
-        GenerateGrid();
+        ChangeState(GameState.GenerateLevel);
+    }
+
+    private void ChangeState(GameState newState)
+    {
+        _state = newState;
+
+        switch (newState)
+        {
+            case GameState.GenerateLevel:
+                GenerateGrid();
+                break;
+            case GameState.SpawningBlocks:
+                SpawnBlocks(_round++ == 0 ? 2 : 1);
+                break;
+            case GameState.WaitingInput:
+                break;
+            case GameState.Moving:
+                break;
+            case GameState.Win:
+                break;
+            case GameState.Lose:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+
+        }
+    }
+
+    private void Update()
+    {
+        if (_state != GameState.WaitingInput) return;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) Shift(Vector2.left);
     }
 
     void GenerateGrid()
     {
+        _round = 0;
         _nodes = new List<Node>();
         _blocks = new List<Block>();
-        for (int x = 0; x <_width; x++)
+        for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
             {
-                var node = Instantiate(_nodePrefab,new Vector2(x,y), Quaternion.identity);
+                var node = Instantiate(_nodePrefab, new Vector2(x, y), Quaternion.identity);
                 _nodes.Add(node);
             }
         }
 
-        var center = new Vector2((float) _width /2 -0.5f,(float) _height / 2 -0.5f);
+        var center = new Vector2((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f);
 
         var board = Instantiate(_boardPrefab, center, Quaternion.identity);
+        board.size = new Vector2(_width,_height);
+
+        Camera.main.transform.position = new Vector3(center.x,center.y,-10);
+
+        ChangeState(GameState.SpawningBlocks);
     }
+
 
     void SpawnBlocks(int amount)
     {
-        var freeNodes = _nodes.Where(n => n.OccupiedBlock == null).OrderBy(b => Random.value);
+        var freeNodes = _nodes.Where(n => n.OccupiedBlock == null).OrderBy(b => Random.value).ToList();
 
-        for (int i = 0; i < amount; i++)
+        foreach (var node in freeNodes.Take(amount))
         {
-            var block = Instantiate(_blockPrefab);
+            var block = Instantiate(_blockPrefab, node.Pos, Quaternion.identity);
+            block.Init(GetBlockTypeByValue(Random.value > 0.8f ?  4 : 2));
+        }
+        
+        if (freeNodes.Count() == 1)
+        {
+            //Lost the Game
+            return;
         }
 
-        if (freeNodes.Count() == 2)
-        {
+        ChangeState(GameState.WaitingInput);
+    }
+    void Shift(Vector2 dir)
+    {
+        var orderedBlocks = _blocks.OrderBy(b => b.Pos.x).ThenBy(b=>b.Pos.y).ToList();
+        if (dir == Vector2.right || dir == Vector2.up) orderedBlocks.Reverse();
 
+        foreach (var block in orderedBlocks)
+        {
+            do
+            {
+
+            } while (block);
         }
     }
+}
+
+[Serializable]
+
+public struct BlockType
+{
+    public int Value;
+    public Color Color;
+}
+
+public enum GameState
+{
+    GenerateLevel,
+    SpawningBlocks,
+    WaitingInput,
+    Moving,
+    Win,
+    Lose
 }
